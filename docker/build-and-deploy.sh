d33a0bf3c65f87ec43a81c7b04691a5b6e8eb340
Innokentiy Sokolov <keshon@zoho.com>
2024-08-02 09:08:47 +0300

    added docker
    added version and build scripts

---
+++ docker/build-and-deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -f .env ]; then
+    source .env
+else
+    echo ".env file not found!"
+    exit 1
+fi
+
+if [ "$GIT" != "false" ]; then
+    rm -rf ./src
+    git clone "$GIT_URL" src
+else
+    if [ ! -d "./src" ]; then
+        echo "src dir not found!"
+        exit 1
+    fi
+fi
+
+docker-compose down
+
+if [ "$(docker images -q "${ALIAS}-image" 2>/dev/null)" ]; then
+    docker rmi "${ALIAS}-image"
+fi
+
+DOCKER_BUILDKIT=1 docker build -t "${ALIAS}-image" .
+
+docker compose -f docker-compose.yml up -d
+docker system prune --all
---
+++ internal/version/version.go
@@ -0,0 +1,9 @@
+package version
+
+var (
+	BuildDate      string
+	GoVersion      string
+	AppName        = "PurgeBot"
+	AppFullName    = "PurgeBot - Autodeletion Discord Bot"
+	AppDescription = "Automatically purging old messages from Discord channels based on specified duration"
+)
